5bd03e7aabc695baeeb8d1193dea204635c40ffd
Samuel Raducan <samuel.thembp@gmail.com>
2022-02-13 19:13:20 +0000

    Add grouped_by_cohort method in directory.rb to display a specific  cohort

--- directory.rb
+++ directory.rb
@@ -26,7 +26,7 @@ def input_students
     cohort = "February".to_sym if cohort == nil
     
     # Add student to students array | display 'student' or 'students' baseed on the size
-    students << { name: name, cohort: cohort }
+    students << { name: name, cohort: cohort.to_sym }
     puts "Now we have #{students.count} #{students.count == 1 ? "student" : "students."}"
   
     puts "Please enter another student. To finish - just hit return twice"
@@ -36,9 +36,18 @@ def input_students
     students
 end
 
+# Print the students grouped by a specific cohort.
+def grouped_by_cohort(students)
+  puts "Please enter the month of the cohort you would like to see: "
+  cohort_to_print = gets.chomp.to_sym
+
+  puts "Students from #{cohort_to_print} cohort: "
+  students.map { |student| puts "#{student[:name]}" if student[:cohort] == cohort_to_print }
+end
+
 def print_header
     puts "The students of Villains Academy"
-    puts "-------------"
+    puts "-------------------------------"
 end
 
 def print(students)
@@ -46,7 +55,6 @@ def print(students)
     students.each.with_index(1) do |student, idx|
         puts "#{idx}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
-    # p students
 end
 
 def print_footer(students)
@@ -54,8 +62,9 @@ def print_footer(students)
 end
 
 students = input_students
-# nothing happens until we call the below methods.
+# Nothing happens until we call the below methods.
 print_header
+# grouped_by_cohort(students)
 print(students)
 print_footer(students)
 
