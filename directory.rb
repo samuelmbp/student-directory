0655ec9324211d30db3180cd10393c1e95a31f3d
Samuel Raducan <samuel.thembp@gmail.com>
2022-02-14 10:30:09 +0000

    Refactor and simplified the code in directory.rb file

--- directory.rb
+++ directory.rb
@@ -1,87 +1,66 @@
-def input_students
-  # Create an empty array
-  students = []
+@students = [] # An empty array accessible to all methods
 
-  puts "Please enter the name and the cohort of the student on the same line with a space between"
-  puts "Press 'quit' if you made a mistake and re-enter student information"
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish - just hit return twice"
   
-  student = " "
+  name = gets.chomp # Gets the first name
   
-  # While name is not empty repeat this code
-  while !student.empty? do
-    # Student information
-    student = gets.chomp.split(" ")
-    
-    # Parallel assignment
-    name, cohort = student
-    
-    # Check for empty value
-    break if name == nil
-    
-    # Quit if user makes a typo
-    break if name == "quit" || cohort == "quit"
-    
-    # Default value if name or cohort is empty  - converted to a symbol
-    name = "Sam".to_sym if name == nil
-    cohort = "February".to_sym if cohort == nil
-    
-    # Add student hash to students array 
-    students << { name: name.capitalize, cohort: cohort.to_sym }
+  # While the name is not empty, repeat this code
+  while !name.empty? do
+    # Add student hash to an array
+    @students << { name: name.capitalize, cohort: :february }
     
     # Display 'student' or 'students' baseed on the size
-    puts "Now we have #{students.count} #{students.count == 1 ? "student" : "students."}"
-  
-    puts "Please enter another student. To finish - just hit return twice"
-  end
+    puts "Now we have #{@students.count} #{@students.count == 1 ? "student" : "students."}"
     
-  # Return array of students
-  students
+    name = gets.chomp # Gets another name from the user
+  end
 end
 
-# Print the students grouped by a specific cohort.
-def grouped_by_cohort(students)
-  puts "Please enter the month of the cohort you would like to see: "
-  cohort_to_print = gets.chomp.to_sym
-
-  puts "Students from #{cohort_to_print} cohort: "
-  students.map { |student| puts "#{student[:name]}" if student[:cohort] == cohort_to_print }
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
 end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit # 'exit' will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again."
+  end
+end  
+  
 
 def print_header
     puts "The students of Villains Academy"
     puts "-------------------------------"
 end
 
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # cause the program to terminate
-    else
-      puts "I don't know what you meant, try again."
-    end
-  end
-end
-
-def print(students)
-  # Display list if it has at least one student
-    if students.count >= 1
-      # Add an index starting from 1 to each student
-      students.each.with_index(1) do |student, idx|
+def print_student_list
+  # Display the list if it has at least one student
+    if @students.count >= 1
+      # Add an index to each student
+      @students.each.with_index(1) do |student, idx|
           puts "#{idx}. #{student[:name]} (#{student[:cohort]} cohort)"
       end
     else
@@ -89,16 +68,8 @@ def print(students)
     end
 end
 
-def print_footer(students)
-    puts "Overall, we have #{students.count} great #{students.count == 1 ? "student" : "students"}"    
+def print_footer
+    puts "Overall, we have #{@students.count} great #{@students.count == 1 ? "student" : "students"}"    
 end
 
 interactive_menu
-
-# students = input_students
-# Nothing happens until we call the below methods.
-# print_header
-# grouped_by_cohort(students)
-# print(students)
-# print_footer(students)
-
